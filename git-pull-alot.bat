9619b2046b3b718037f8ef425ebab73faf916b5a
Rojar Smith <rojarsmith@gmail.com>
2023-12-07 16:51:12 +0800

    feat: Git Pull All Projects

---
+++ git-pull-alot.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set "originalPath=%CD%"
+
+cd ..
+
+for /r %%d in (.) do (
+    if exist "%%d\.git" (
+        echo Found .git in %%d
+        pushd "%%d"
+        git pull
+        popd
+    )
+)
+
+cd %originalPath%
+
+echo Done.
