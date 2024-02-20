6b03c686adabc4bbc5c7f77d02532de060dc9910
Rojar Smith <rojarsmith@gmail.com>
2024-02-20 20:26:37 +0800

    feat: Git Clone All Projects

---
+++ git-clone-alot.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set "originalPath=%CD%"
+set "repoList=%originalPath%\git-clone-alot.txt"
+
+cd ..
+
+:: Check exist
+if not exist "%repoList%" (
+    echo Repository list file %repoList% not found.
+    goto End
+)
+
+:: Read each line
+for /F "tokens=*" %%G in ('type "%repoList%"') do (
+    set "gitURL=%%G"
+    setlocal enabledelayedexpansion
+    :: Fetch the last of Git URL part for repo name
+    for %%H in ("!gitURL!") do (
+        set "repoName=%%~nH"
+        set "repoPath=%%~dpH"
+    )
+    :: Check repo exist
+    if not exist "!repoName!\" (
+        echo Cloning !gitURL! into !repoName!
+        git clone "!gitURL!"
+    ) else (
+        echo Repository !repoName! already exists.
+    )
+    endlocal
+)
+
+:End
+cd "%originalPath%"
+echo Done.
