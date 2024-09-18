970c151ef987ccfbbcc885b08988730c65e16e67
Rojar Smith <rojarsmith@gmail.com>
2024-09-18 19:10:06 +0800

    feat: Add GTK Signal

--- template/gtk4-cmake/src/main.c
+++ template/gtk4-cmake/src/main.c
@@ -1,7 +1,26 @@
 #include <gtk/gtk.h>
 
-static void on_activate(GtkApplication *app, gpointer user_data) {
+static int counter = 0;
+
+static void on_button_clicked(GtkWidget *widget, gpointer label)
+{
+    counter++;
+
+    char label_text[50];
+    snprintf(label_text, sizeof(label_text), "%d", counter);
+    gtk_label_set_text(GTK_LABEL(label), label_text);
+
+    if (counter >= 10)
+    {
+        counter = -1;
+    }
+}
+
+static void on_activate(GtkApplication *app, gpointer user_data)
+{
     GtkWidget *window;
+    GtkWidget *vbox;
+    GtkWidget *label;
     GtkWidget *button;
 
     // create a window
@@ -9,15 +28,24 @@ static void on_activate(GtkApplication *app, gpointer user_data) {
     gtk_window_set_title(GTK_WINDOW(window), "GTK4");
     gtk_window_set_default_size(GTK_WINDOW(window), 192, 108);
 
+    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_window_set_child(GTK_WINDOW(window), vbox);
+
+    label = gtk_label_new("0");
+    gtk_box_append(GTK_BOX(vbox), label);
+
     // create a button
-    button = gtk_button_new_with_label("Hello World");
-    gtk_window_set_child(GTK_WINDOW(window), button);
+    button = gtk_button_new_with_label("+1");
+    gtk_box_append(GTK_BOX(vbox), button);
+
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), label);
 
     // show the window
     gtk_window_present(GTK_WINDOW(window));
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     GtkApplication *app;
     int status;
 
